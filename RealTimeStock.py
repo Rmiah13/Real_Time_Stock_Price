04a5ba4bff9886af585b7d0d8ab9937b2eefd7e5
Rmiah13 <36214752+Rmiah13@users.noreply.github.com>
2020-07-27 10:43:04 +0100

    Add files via upload

---
+++ RealTimeStock.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import time
+
+#Only works with US stocks at the moment
+api_key = 'VSKPIUDFJEEYHJTM'
+
+def get_price():
+    print('Enter the stocks ticker')
+    ticker = input()
+
+    ts = TimeSeries(key=api_key, output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol=ticker,interval = '1min', outputsize='compact')
+    close_data = data['4. close']
+    last_change = close_data[-1]
+
+    #print(data)
+    print('$',last_change)
+
+    repeat = input('Would you like to enter another ticker? ').lower()
+    if repeat == 'yes':
+        get_price()
+    else:
+        print('Thanks for using the app ' )
+
+get_price()
